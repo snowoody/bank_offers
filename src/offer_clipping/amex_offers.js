3ab0924e7c4a33d8fb6e53757044653101cf0710
snowoody <thu.woody@gmail.com>
2025-09-14 12:56:28 -0700

    update amex offers

--- src/offer_clipping/amex_offers.js
+++ src/offer_clipping/amex_offers.js
@@ -1,1 +1,4 @@
-javascript: (async () => { var offerButtons = Array.from(document.getElementsByClassName("_btn_jsa6y_25 _secondary_jsa6y_110 fluid-sm-down margin-auto-l text-nowrap")).filter(btn => btn.title == "Add to Card"); for (let i = 0; i < offerButtons.length; i++) { console.log("Clicking offer button"); offerButtons[i].click(); await new Promise(r => setTimeout(r, 2000)); } })();
+// outdated 
+// javascript: (async () => { var offerButtons = Array.from(document.getElementsByClassName("_btn_jsa6y_25 _secondary_jsa6y_110 fluid-sm-down margin-auto-l text-nowrap")).filter(btn => btn.title == "Add to Card"); for (let i = 0; i < offerButtons.length; i++) { console.log("Clicking offer button"); offerButtons[i].click(); await new Promise(r => setTimeout(r, 2000)); } })();
+// current
+javascript: (async () => { var offerButtons = Array.from(document.querySelectorAll('button[data-testid="merchantOfferListAddButton"]')); for (let i = 0; i < offerButtons.length; i++) { console.log("Clicking offer button " + (i+1) + "/" + offerButtons.length); offerButtons[i].click(); await new Promise(r => setTimeout(r, Math.random() * 811 + 500)); } })();
